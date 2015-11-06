1e5b5d7ee3ca3cd636ae8cc1a053b16013e78246
Yi Liu <purplesky8985@gmail.com>
2015-11-06 15:41:01 -0600

    added form validation

--- app/src/main/java/com/example/yiliu/forecast/MainActivity.java
+++ app/src/main/java/com/example/yiliu/forecast/MainActivity.java
@@ -7,13 +7,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -22,6 +27,12 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     public static final String QUERY = "com.example.yiliu.forecast.STREET";
+    private EditText streetEditText;
+    private TableRow streetValid;
+    private EditText cityEditText;
+    private TableRow cityValid;
+    private Spinner stateSpinner;
+    private TableRow stateValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 R.array.state_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         state.setAdapter(adapter);
+
+        addValidateListener();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -69,28 +82,128 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void submitForm(View view) throws MalformedURLException {
+    public void submitForm(View view) {
+        boolean valid = true;
 
+        // get the input information
         String street = ((EditText) findViewById(R.id.street)).getText().toString();
         String city = ((EditText) findViewById(R.id.city)).getText().toString();
         String state = ((Spinner) findViewById(R.id.state)).getSelectedItem().toString();
         String degree = ((RadioGroup) findViewById(R.id.degree))
                 .getCheckedRadioButtonId() == 0? "us": "si";
 
-        Uri.Builder url = new Uri.Builder();
-        url.scheme("http");
-        url.authority("www.glistenlau.com");
-        url.appendPath("forecast");
-        url.appendPath("api");
-        url.appendPath("weather");
-        url.appendQueryParameter("street", street);
-        url.appendQueryParameter("city", city);
-        url.appendQueryParameter("state", state);
-        url.appendQueryParameter("degreeType", degree);
+        // validate the form input
+        if (street.trim().length() == 0) {
+            streetValid.setVisibility(View.VISIBLE);
+            valid = false;
+        }
+        if (city.trim().length() == 0) {
+            cityValid.setVisibility(View.VISIBLE);
+            valid = false;
+        }
+        if (state.equals("Select your state...")) {
+            stateValid.setVisibility(View.VISIBLE);
+            valid = false;
+        }
 
+        // if valid query the weather
+        if (valid) {
+            // generate the query url
+            Uri.Builder url = new Uri.Builder();
+            url.scheme("http");
+            url.authority("www.glistenlau.com");
+            url.appendPath("forecast");
+            url.appendPath("api");
+            url.appendPath("weather");
+            url.appendQueryParameter("street", street);
+            url.appendQueryParameter("city", city);
+            url.appendQueryParameter("state", state);
+            url.appendQueryParameter("degreeType", degree);
 
-        Intent intent = new Intent(this, DisplayWeatherActivity.class);
-        intent.putExtra(QUERY, url.toString());
-        startActivity(intent);
+            Intent intent = new Intent(this, DisplayWeatherActivity.class);
+            intent.putExtra(QUERY, url.toString());
+            startActivity(intent);
+        }
     }
+
+    private void addValidateListener() {
+        // get the views of the form
+        streetEditText = (EditText) findViewById(R.id.street);
+        streetValid = (TableRow) findViewById(R.id.streetValid);
+        cityEditText = (EditText) findViewById(R.id.city);
+        cityValid = (TableRow) findViewById(R.id.cityValid);
+        stateSpinner = (Spinner) findViewById(R.id.state);
+        stateValid = (TableRow) findViewById(R.id.stateValid);
+
+        // set validate watcher to form input
+        streetEditText.addTextChangedListener(streetWatcher);
+        cityEditText.addTextChangedListener(cityWatcher);
+        stateSpinner.setOnItemSelectedListener(sateWatcher);
+    }
+
+    private final TextWatcher streetWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (s.length() == 0) {
+                streetValid.setVisibility(View.INVISIBLE);
+            } else {
+                if (s.toString().trim().length() == 0) {
+                    streetValid.setVisibility(View.VISIBLE);
+                } else {
+                    streetValid.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+    };
+
+    private final TextWatcher cityWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (s.length() == 0) {
+                cityValid.setVisibility(View.INVISIBLE);
+            } else {
+                if (s.toString().trim().length() == 0) {
+                    cityValid.setVisibility(View.VISIBLE);
+                } else {
+                    cityValid.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+    };
+
+    private final AdapterView.OnItemSelectedListener sateWatcher = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selected = parent.getItemAtPosition(position).toString();
+            if (!selected.equals("Select your state...")) {
+                stateValid.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
 }
